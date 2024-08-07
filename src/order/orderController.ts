d75d9d4b192ccc0ebc435aad8c6ebf2b9487d4be
NareshDhamu <nareshsu427@gmail.com>
2024-08-07 11:31:19 +0530

    calculate subtotal

--- src/order/orderController.ts
+++ src/order/orderController.ts
@@ -1,10 +1,199 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import {
+  CartItem,
+  ProductPricingCache,
+  Topping,
+  ToppingPricingCache,
+} from "../types";
+import productCacheModel from "../productCache/productCacheModel";
+import toppingCacheModel from "../toppingCache/toppingCacheModel";
 
 export class OrderController {
   create = async (req: Request, res: Response) => {
-    return res.send({ success: true });
+    const isEmpty = validationResult(req).isEmpty();
+    if (!isEmpty) {
+      return res.status(400).send({ errors: validationResult(req).array() });
+    }
+    const totalPrice = await this.calculateTotal(req.body.cart);
+    return res.send({ totalPrice: totalPrice });
   };
+
+  private calculateTotal = async (cart: CartItem[]) => {
+    const productIds = cart.map((item) => item._id);
+    const productPricings = await productCacheModel.find({
+      productId: {
+        $in: productIds,
+      },
+    });
+    const cartToppingIds = cart.reduce((acc, item) => {
+      return [
+        ...acc,
+        ...item.chosenConfiguration.selectedToppings.map(
+          (topping) => topping._id,
+        ),
+      ];
+    }, []);
+
+    const toppingPricings = await toppingCacheModel.find({
+      toppingId: {
+        $in: cartToppingIds,
+      },
+    });
+
+    const totalPrice = cart.reduce((acc, curr) => {
+      const cachedProductPrice = productPricings.find(
+        (product) => product.productId === curr._id,
+      );
+      return (
+        acc +
+        curr.qty * this.getItemTotal(curr, cachedProductPrice, toppingPricings)
+      );
+    }, 0);
+
+    return totalPrice;
+  };
+
+  private getItemTotal = (
+    item: CartItem,
+    cachedProductPrice: ProductPricingCache | undefined,
+    toppingPricing: ToppingPricingCache[],
+  ) => {
+    // Calculate the total price of selected toppings
+    const toppingsTotal = item.chosenConfiguration.selectedToppings.reduce(
+      (acc, curr) => {
+        return acc + this.getCurrentToppingPrice(curr, toppingPricing);
+      },
+      0,
+    );
+
+    // Ensure cachedProductPrice is defined before accessing priceConfiguration
+    if (!cachedProductPrice || !cachedProductPrice.priceConfiguration) {
+      console.error(
+        `No pricing information found for product with ID ${item._id}`,
+      );
+      return toppingsTotal; // Return just the toppings total if no product price is found
+    }
+
+    // Calculate the total price based on the price configuration of the product
+    const productTotal = Object.entries(
+      item.chosenConfiguration.priceConfiguration,
+    ).reduce((acc, [key, value]) => {
+      const price =
+        cachedProductPrice.priceConfiguration[key].availableOptions[value];
+      return acc + price;
+    }, 0);
+
+    return productTotal + toppingsTotal;
+  };
+
+  private getCurrentToppingPrice = (
+    topping: Topping,
+    toppingPricings: ToppingPricingCache[],
+  ) => {
+    const currentTopping = toppingPricings.find(
+      (current) => topping._id === current.toppingId,
+    );
+    if (!currentTopping) {
+      return topping.price;
+    }
+    return currentTopping.price;
+  };
+
   get = async (req: Request, res: Response) => {
     return res.send({});
   };
 }
+
+// import { Request, Response } from "express";
+// import { validationResult } from "express-validator";
+// import {
+//   CartItem,
+//   ProductPricingCache,
+//   Topping,
+//   ToppingPricingCache,
+// } from "../types";
+// import productCacheModel from "../productCache/productCacheModel";
+// import toppingCacheModel from "../toppingCache/toppingCacheModel";
+
+// export class OrderController {
+//   create = async (req: Request, res: Response) => {
+//     const isEmpty = validationResult(req).isEmpty();
+//     if (!isEmpty) {
+//       return res.status(400).send({ errors: validationResult(req).array() });
+//     }
+//     const totalPrice = await this.calculateTotal(req.body.cart);
+//     return res.send({ totalPrice: totalPrice });
+//   };
+//   private calculateTotal = async (cart: CartItem[]) => {
+//     const productIds = cart.map((item) => item._id);
+//     const productPricings = await productCacheModel.find({
+//       productId: {
+//         $in: productIds,
+//       },
+//     });
+//     const cartToppingIds = cart.reduce((acc, item) => {
+//       return [
+//         ...acc,
+//         ...item.chosenConfiguration.selectedToppings.map(
+//           (topping) => topping._id,
+//         ),
+//       ];
+//     }, []);
+
+//     const toppingPricings = await toppingCacheModel.find({
+//       toppingId: {
+//         $in: cartToppingIds,
+//       },
+//     });
+
+//     const totalPrice = cart.reduce((acc, curr) => {
+//       const cachedProductPrice = productPricings.find(
+//         (product) => product.productId === curr._id,
+//       );
+//       return (
+//         acc +
+//         curr.qty * this.getItemTotal(curr, cachedProductPrice, toppingPricings)
+//       );
+//     }, 0);
+
+//     return totalPrice;
+//   };
+
+//   private getItemTotal = (
+//     item: CartItem,
+//     cachedProductPrice: ProductPricingCache,
+//     toppingPricing: ToppingPricingCache[],
+//   ) => {
+//     const toppingsTotal = item.chosenConfiguration.selectedToppings.reduce(
+//       (acc, curr) => {
+//         return acc + this.getCurrentToppingPrice(curr, toppingPricing);
+//       },
+//       0,
+//     );
+//     const productTotal = Object.entries(
+//       item.chosenConfiguration.priceConfiguration,
+//     ).reduce((acc, [key, value]) => {
+//       const price =
+//         cachedProductPrice.priceConfiguration[key].availableOptions[value];
+//       return acc + price;
+//     }, 0);
+//     return productTotal + toppingsTotal;
+//   };
+
+//   private getCurrentToppingPrice = (
+//     topping: Topping,
+//     toppingPricings: ToppingPricingCache[],
+//   ) => {
+//     const currentTopping = toppingPricings.find(
+//       (current) => topping._id === current.toppingId,
+//     );
+//     if (!currentTopping) {
+//       return topping.price;
+//     }
+//     return currentTopping.price;
+//   };
+//   get = async (req: Request, res: Response) => {
+//     return res.send({});
+//   };
+// }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import { Request } from "express";
+import mongoose from "mongoose";
 
 export type AuthCookie = {
   accessToken: string;
@@ -30,6 +31,7 @@ export interface ProductPricingCache {
   priceConfiguration: PriceConfiguration;
 }
 export interface ToppingPricingCache {
+  _id: mongoose.Types.ObjectId;
   toppingId: string;
   price: number;
 }
@@ -41,4 +43,36 @@ export interface ProductMessage {
 export interface ToppingMessage {
   id: string;
   price: number;
+}
+
+export type ProductPriceConfiguration = {
+  [key: string]: {
+    priceType: "base" | "aditional";
+    availableOptions: {
+      [key: string]: number;
+    };
+  };
+};
+export type Product = {
+  _id: string;
+  name: string;
+  image: string;
+  description: string;
+  priceConfiguration: ProductPriceConfiguration;
+};
+export type Topping = {
+  _id: string;
+  name: string;
+  image: string;
+  price: number;
+};
+export interface CartItem
+  extends Pick<Product, "_id" | "name" | "image" | "priceConfiguration"> {
+  chosenConfiguration: {
+    priceConfiguration: {
+      [key: string]: string;
+    };
+    selectedToppings: Topping[];
+  };
+  qty: number;
 }
