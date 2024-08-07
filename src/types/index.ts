7a75b4374197c9a451a841382878388ebbd801e9
NareshDhamu <nareshsu427@gmail.com>
2024-08-07 09:24:30 +0530

    kafka setUp

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,3 +17,28 @@ export interface paginateQuery {
   page: number;
   limit: number;
 }
+
+export interface PriceConfiguration {
+  priceType: "base" | "aditional";
+  availableOptions: {
+    [key: string]: number;
+  };
+}
+
+export interface ProductPricingCache {
+  productId: string;
+  priceConfiguration: PriceConfiguration;
+}
+export interface ToppingPricingCache {
+  toppingId: string;
+  price: number;
+}
+
+export interface ProductMessage {
+  id: string;
+  priceConfiguration: PriceConfiguration;
+}
+export interface ToppingMessage {
+  id: string;
+  price: number;
+}
